7fe114843f3daf0924632dc9e7c516fb717c319a
Cormac Levins <cormac.levins@axonitsa.com>
2021-06-10 16:20:33 +0100

    init commit

---
+++ strapi/config/server.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  production: true,
+  proxy: true,
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a9c198cc16a3515ed0fee880ebf84a2'),
+    },
+  },
+});
