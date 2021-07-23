8d11bc72a523f2954590fd95aba8f9eaf8b74a17
Cormac Levins <cormac.levins@axonitsa.com>
2021-07-23 15:06:05 +0100

    Update prod server name on nginx

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 
 export default function AboutMe() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <h2>About Me</h2>
     </>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import Navbar from '@components/Navbar';
 import Hero from '@components/Hero';
-import getConfig from 'next/config';
 
-const { serverRuntimeConfig } = getConfig();
-
-export default function Index({ data }) {
+export default function Index({ data }: any) {
   return (
     <>
       <Navbar socialLinks={data.home_social_links} />
@@ -15,15 +12,14 @@ export default function Index({ data }) {
         mobileHeroUrl={data.home_hero.mobileImage.url}
       />
       <h2>Index</h2>
-      {console.log(serverRuntimeConfig.apiBaseUrl)}
     </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(`${serverRuntimeConfig.apiBaseUrl}/home`);
-
-  console.log(serverRuntimeConfig.apiBaseUrl);
+  const res = await fetch(
+    `http://sub.pete-guay-athletic-therapy.eu-west-1.elasticbeanstalk.com/home`,
+  );
 
   const data = await res.json();
 
